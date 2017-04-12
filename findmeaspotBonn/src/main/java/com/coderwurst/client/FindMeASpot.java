f58633454afdecd62c608639b1709c2eaa2dc725
coderwurst <coderwurst@gmail.com>
2017-04-12 21:06:16 +0200

    initial web ui

--- findmeaspotBonn/src/main/java/com/coderwurst/client/FindMeASpot.java
+++ findmeaspotBonn/src/main/java/com/coderwurst/client/FindMeASpot.java
@@ -1,11 +1,12 @@
 package com.coderwurst.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
 
 public class FindMeASpot implements EntryPoint {
 
 	
 	public void onModuleLoad() {
-
+		RootPanel.get().add(new FindMeASpotBonn());
 	}
 }
