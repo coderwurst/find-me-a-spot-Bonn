3c18ba814304e398967c36804926dc94a60eff7b
Andrew McMechan <coderwurst@users.noreply.github.com>
2019-04-28 14:15:12 +0200

    initial car park images

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,3 +1,16 @@
 export const elements = {
     carParkList: document.querySelector('.carpark-list')
 };
+
+export const images = {
+    bahnhof: 'http://bcp-bonn.de/wp-content/uploads/bahnhof.jpg',
+    beethoven: 'http://bcp-bonn.de/wp-content/uploads/beethovenparkhaus-2.jpg',
+    beetplatz: 'http://bcp-bonn.de/wp-content/uploads/beethovenparkhaus3-1.jpg',
+    friedensplatz: 'http://bcp-bonn.de/wp-content/uploads/Friedensplatzgarage.jpg',
+    karstadt: 'https://s3-media1.fl.yelpcdn.com/bphoto/fHCoFA-3SlZYeF7YB1jLaw/ls.jpg',
+    markt: 'http://bcp-bonn.de/marktgarage/',
+    muensterplatz: 'http://bcp-bonn.de/wp-content/uploads/munsterplatz3.jpg',
+    rheinaue: 'http://www.general-anzeiger-bonn.de/bonn/stadt-bonn/Flohmarkt-Rheinaue-Parkplatz-article3632812.html/ALTERNATES/v4_3_w620/Flohmarkt%20Rheinaue%20Parkplatz?ts=1503310257000',
+    stadthaus: 'http://bcp-bonn.de/stadthausgarage/',
+    stadthaus_unten: 'http://bcp-bonn.de/stadthausgarage/'
+}
